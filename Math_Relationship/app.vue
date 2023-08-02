f34d07540756aa1ea5d226cbc76e37604d553b45
goodnight <victor.tonu@yahoo.com>
2023-08-01 22:51:20 -0700

    Added files
    Added files & buttons

--- Math_Relationship/app.vue
+++ Math_Relationship/app.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <UContainer class="m-8 mx-8 border border-1 border-gray-300 rounded-lg"
+    ><UButton
+      icon="i-heroicons-arrow-right-20-solid"
+      size="sm"
+      color="primary"
+      variant="solid"
+      label="TEST"
+      :trailing="true"
+      class="m-2"
+    />
+
+    <UButton
+      icon="i-heroicons-arrow-path-rounded-square-solid"
+      size="sm"
+      color="primary"
+      variant="solid"
+      label="RESET"
+      :trailing="true"
+      class="m-2"
+    />
+  </UContainer>
 </template>
